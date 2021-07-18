a847448d39e8b7ecdb1409ffcb9a498ff378773e
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2021-07-18 22:44:19 +0200

    check if registration is ok

--- controllers/users.js
+++ controllers/users.js
@@ -9,17 +9,27 @@ const checkUsers = (users) => {
 
 const addUser = (users) => {
     return (req, res) => {
+        const userEmail = req.body.userEmail
         const usrnm = req.body.username
         const userId = req.body.userId
         const passwd = req.body.password
+        if (!usrnm || !userId || !passwd || !userEmail) {
+            res.status(400).json({error: 'Wrong parameters'})
+            return
+        }
 
         const usernameTaken = Object.values(users).map(user => user.username).some(username => username === usrnm)
         if (usernameTaken) {
             res.status(400).json({error: 'Username taken'})
             return
         }
-        
+        const emailTaken = Object.values(users).map(user => user.email).some(email => email === userEmail)
+        if (emailTaken) {
+            res.status(400).json({error: 'Email taken'})
+            return
+        }
         let user = {
+            email: userEmail,
             username: usrnm,
             password: passwd
           };
@@ -32,13 +42,19 @@ const addUser = (users) => {
 const updateUser = (users) => {
     return (req, res) => {
         const userId = req.params.userId
-      
         if(!userId){
           res.status(400).json({})
           return
         }
         
         const usrname = req.body.name
+
+        const usernameTaken = Object.values(users).map(user => user.username).some(username => username === usrname)
+        if (usernameTaken) {
+            res.status(400).json({error: 'Username taken'})
+            return
+        }
+
         if(usrname){
             users[userId].username = usrname
         }
